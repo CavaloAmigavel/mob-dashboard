b9a89e6a30ee99cedd784cf85bee01abc1a7c124
Carolina Antunes <carolinapazeiro@gmail.com>
2024-05-14 20:23:53 +0100

    mini update

--- src/components/card/AEContainer.vue
+++ src/components/card/AEContainer.vue
@@ -1,70 +1,70 @@
 <template>
-  <div class="container-fluid d-flex m-2">
-    <div class="card" style="width: 200px">
-      <div class="card-body">
-        <h4 class="card-title">{{ ae_ri }}</h4>
-        <div v-if="cons">
-          <ConContainer
-            v-for="(con, index) in conArray"
-            :key="index"
-            :con="con"
-            :ae="ae"
-          />
-        </div>
-      </div>
-    </div>
-  </div>
+	<div class="container-fluid d-flex m-2">
+		<div class="card" style="width: 300px">
+			<div class="card-body">
+				<h4 class="card-title">{{ ae_ri }}</h4>
+				<div v-if="cons">
+					<ConContainer
+						v-for="(con, index) in conArray"
+						:key="index"
+						:con="con"
+						:ae="ae"
+					/>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import ConContainer from "./ConContainer.vue";
 export default {
-  name: "AEContainer",
-  components: {
-    ConContainer,
-  },
-  props: {
-    ae: {
-      type: String,
-      required: true,
-    },
-  },
-  data() {
-    return {
-      cons: null,
-    };
-  },
-  computed: {
-    //AE Resource Indentifier
-    ae_ri() {
-      return this.ae.split("/")[1];
-    },
-    conArray() {
-      return this.cons["m2m:uril"].map((uri) => uri.split("/").pop());
-    },
-  },
+	name: "AEContainer",
+	components: {
+		ConContainer,
+	},
+	props: {
+		ae: {
+			type: String,
+			required: true,
+		},
+	},
+	data() {
+		return {
+			cons: null,
+		};
+	},
+	computed: {
+		//AE Resource Indentifier
+		ae_ri() {
+			return this.ae.split("/")[1];
+		},
+		conArray() {
+			return this.cons["m2m:uril"].map((uri) => uri.split("/").pop());
+		},
+	},
 
-  methods: {
-    fetchCON() {
-      const options = {
-        method: "GET",
-        headers: {
-          "X-M2M-Origin": "CAdmin",
-          "X-M2M-RI": "123",
-          "X-M2M-RVI": "3",
-        },
-      };
-      fetch(`/acme${this.ae}?fu=1&ty=3`, options)
-        .then((response) => response.json())
-        .then((response) => {
-          console.log(response);
-          this.cons = response;
-        })
-        .catch((err) => console.error(err));
-    },
-  },
-  mounted() {
-    this.fetchCON();
-  },
+	methods: {
+		fetchCON() {
+			const options = {
+				method: "GET",
+				headers: {
+					"X-M2M-Origin": "CAdmin",
+					"X-M2M-RI": "123",
+					"X-M2M-RVI": "3",
+				},
+			};
+			fetch(`/acme${this.ae}?fu=1&ty=3`, options)
+				.then((response) => response.json())
+				.then((response) => {
+					console.log(response);
+					this.cons = response;
+				})
+				.catch((err) => console.error(err));
+		},
+	},
+	mounted() {
+		this.fetchCON();
+	},
 };
 </script>
