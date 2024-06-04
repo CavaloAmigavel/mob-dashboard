61351826dd1f9f8cc0f511a041fe8c0bacc62919
Carolina Antunes <carolinapazeiro@gmail.com>
2024-06-04 19:26:13 +0100

    created store.js

---
+++ src/stores/store.js
@@ -0,0 +1,14 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useCounterStore = defineStore("counter", () => {
+	const count = ref(0);
+	const name = ref("Message");
+	const doubleCount = computed(() => count.value * 2);
+
+	function increment() {
+		count.value++;
+	}
+
+	return { count, increment, name, doubleCount };
+});
